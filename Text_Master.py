77e58d6691b6876312974f216e6f5a3d93aea308
HsnSalman <hssalman93@gmail.com>
2024-06-19 03:40:34 +0300

    update

--- Text_Master.py
+++ Text_Master.py
@@ -1,34 +1,32 @@
-import streamlit as st  
-from SimplerLLM.language.llm import LLM, LLMProvider  
-from SimplerVectors_core import VectorDatabase  
-import tempfile  
-import fitz  
-import docx  
-from PIL import Image  
+import streamlit as st
+from SimplerLLM.language.llm import LLM, LLMProvider
+from SimplerVectors_core import VectorDatabase
+import tempfile
+import fitz
+import docx
+from PIL import Image
 from embeddings import generate_embeddings_open_ai, generate_text
 from chunker import chunk_by_max_chunk_size
 import os
 from openai import OpenAI
+import pyttsx3 # type: ignore
 
 # Set up the Streamlit app configuration
-st.set_page_config(page_title="Text Summarizer", page_icon=":memo:")
+st.set_page_config(page_title="Text Master", page_icon=":memo:")
 
 # Sidebar setup
-st.sidebar.title("Model Selection and Options")
-model_choice = st.sidebar.selectbox("Select LLM Model", ["OpenAI GPT-3.5-turbo", "OpenAI GPT-4", "Gemini"])
-st.sidebar.markdown("<hr>", unsafe_allow_html=True)
+with st.sidebar:
+    st.title("Model Selection")
+    model_choice = st.selectbox("Select LLM Model", ["OpenAI GPT-3.5-turbo", "OpenAI GPT-4", "Gemini"])
+    st.markdown("<hr>", unsafe_allow_html=True)
+    api_key = st.text_input("Enter your API key", type="password")
 
-# Add API key input field
-api_key = st.sidebar.text_input("Enter your API key", type="password")
+    if not api_key:
+        st.warning("Please enter your API key to proceed.")
+        st.stop()
 
-# Check if the API key is provided
-if not api_key:
-    st.sidebar.warning("Please enter your API key to proceed.")
-    st.stop()
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
-
-openai_client = OpenAI(api_key=OPENAI_API_KEY)
+# Initialize OpenAI client
+openai_client = OpenAI(api_key=api_key)
 
 # Initialize the selected language model
 if model_choice == "OpenAI GPT-3.5-turbo":
@@ -38,23 +36,32 @@ elif model_choice == "OpenAI GPT-4":
     model_name = "gpt-4"
     Phd = LLM.create(LLMProvider.OPENAI, model_name=model_name)
 elif model_choice == "Gemini":
-    # Replace 'gemini-model' with the actual model name for Gemeni
     model_name = "gemini-pro"
     Phd = LLM.create(LLMProvider.GEMINI, model_name=model_name)
 
-# Set up the Streamlit app title and description with an icon
-header_image = Image.open("textsummarizer.png")  # Replace with your image path
-st.image(header_image, use_column_width=True, caption = "Developed by Hassan Salman")
-st.title("Text Summarizer with LLM Models for Our PhD Team Members")
-st.write("Upload a text file (PDF, TXT, DOCX) and get a summarized version of its content using the selected LLM model.")
+# Page title and header
+st.image("textmaster.png", use_column_width=True, caption="Developed by Hassan Salman")
+st.markdown("<h1 style='text-align: left;'>Text Master</h1>", unsafe_allow_html=True)
+st.subheader("Your Gateway to Advanced Text Processing")
 
-# Add a line separator
-st.markdown("<hr>", unsafe_allow_html=True)
+st.markdown("""
+    <style>
+    .header-title { font-size: 36px; font-weight: bold; color: #4CAF50; }
+    .subheader-title { font-size: 24px; font-weight: bold; color: #2F4F4F; }
+    .footer-text { font-size: 14px; color: #808080; text-align: center; }
+    .centered-footer { display: flex; justify-content: center; }
+    .dark-blue { color: #141C46; }
+    .alert { background-color: #f8f9fa; color: #6c757d; padding: 10px; border-radius: 5px; border: 1px solid #ced4da; margin-bottom: 20px; }
+    .btn-container { display: flex; flex-direction: column; align-items: center; }
+    .btn-container > button { width: 100%; margin: 5px 0; }
+    </style>
+""", unsafe_allow_html=True)
 
-# Allow users to upload a file
+st.markdown('<div class="header-title dark-blue">Upload a text file (PDF, TXT, DOCX) and get a summarized version of its content using the selected LLM model.</div>', unsafe_allow_html=True)
+
+# File uploader
 uploaded_file = st.file_uploader("Choose a file", type=["pdf", "txt", "docx"], help="Upload a PDF, TXT, or DOCX file for summarization")
 
-# Function to read text files with different encodings
 def read_text_file(file_path, encodings=["utf-8", "latin1", "cp1252"]):
     for encoding in encodings:
         try:
@@ -64,7 +71,6 @@ def read_text_file(file_path, encodings=["utf-8", "latin1", "cp1252"]):
             continue
     raise ValueError("Unable to decode text file with provided encodings")
 
-# Function to read PDF files
 def read_pdf_file(file_path):
     doc = fitz.open(file_path)
     text = ""
@@ -72,7 +78,6 @@ def read_pdf_file(file_path):
         text += page.get_text()
     return text
 
-# Function to read DOCX files
 def read_docx_file(file_path):
     doc = docx.Document(file_path)
     text = ""
@@ -80,15 +85,17 @@ def read_docx_file(file_path):
         text += paragraph.text + "\n"
     return text
 
-# Check if a file has been uploaded
+def generate_tts(text, filename="output.mp3"):
+    engine = pyttsx3.init()
+    engine.save_to_file(text, filename)
+    engine.runAndWait()
+
 if uploaded_file is not None:
-    # Save the uploaded file to a temporary location
     with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
         tmp_file.write(uploaded_file.read())
         tmp_file_path = tmp_file.name
 
     try:
-        # Determine the file type and read the content accordingly
         if uploaded_file.type == "text/plain":
             content = read_text_file(tmp_file_path)
         elif uploaded_file.type == "application/pdf":
@@ -98,102 +105,77 @@ if uploaded_file is not None:
         else:
             raise ValueError("Unsupported file type")
 
-        # Display a preview of the uploaded content
-        st.subheader("File Content Preview")
+        st.markdown('<div class="subheader-title">File Content Preview</div>', unsafe_allow_html=True)
         st.text_area("File Content", content, height=300)
 
-        # Customization options
-        st.subheader("Customize Your Summary")
+        st.markdown('<div class="subheader-title">Customize Your Summary</div>', unsafe_allow_html=True)
         summary_length = st.slider("Summary Length (in sentences)", min_value=1, max_value=10, value=5)
-
-        # Generate the summary using the language model
         summarized_text = Phd.generate_response(prompt=f"Summarize the following text in {summary_length} sentences: {content}")
 
-        # Display the summarized text in the Streamlit app
-        st.subheader("Summarized Text")
+        st.markdown('<div class="subheader-title">Summarized Text</div>', unsafe_allow_html=True)
         st.write(summarized_text)
         
-        # Add a download button for the summarized text
-        st.download_button(
-            label="Download Summary",
-            data=summarized_text,
-            file_name="summary.txt",
-            mime="text/plain"
-        )
+        st.download_button(label="Download Summary", data=summarized_text, file_name="summary.txt", mime="text/plain", key="download-summary-btn")
 
-        # Advanced Text Analysis Options
-        st.subheader("Advanced Text Analysis")
-        if st.button("Perform Sentiment Analysis"):
+        st.markdown('<div class="subheader-title">Advanced Text Analysis</div>', unsafe_allow_html=True)
+        st.markdown('<div class="btn-container">', unsafe_allow_html=True)
+
+        if st.button("Perform Sentiment Analysis", key="sentiment-analysis-btn"):
             sentiment_result = Phd.generate_response(prompt=f"Perform sentiment analysis on the following text: {content}")
             st.write("Sentiment Analysis Result")
             st.write(sentiment_result)
-        
-        if st.button("Extract Keywords"):
+
+        if st.button("Keywords Extraction", key="extract-keywords-btn"):
             keywords_result = Phd.generate_response(prompt=f"Extract keywords from the following text: {content}")
             st.write("Keywords Extraction Result")
             st.write(keywords_result)
 
-        if st.button("Language Translation"):
-            Translated_Results = Phd.generate_response(prompt=f"Translate the following text into French: {content}")
+        if st.button("Language Translation", key="language-translation-btn"):
+            translated_results = Phd.generate_response(prompt=f"Translate the following text into French: {content}")
             st.write("Translated Text")
-            st.write(Translated_Results)
+            st.write(translated_results)
 
-        if st.button("Summarized Text Translation"):
-            Summarized_Translated_Results = Phd.generate_response(prompt=f"Translate the following text into French: {summarized_text}")
+        if st.button("Summarized Text Translation", key="summarized-text-translation-btn"):
+            summarized_translated_results = Phd.generate_response(prompt=f"Translate the following text into French: {summarized_text}")
             st.write("Translated Summarized Text")
-            st.write(Summarized_Translated_Results)
+            st.write(summarized_translated_results)
 
-        # Add a section to ask questions from the PDF
-        st.subheader("Ask Questions from the PDF")
+        if st.button("Summarized Text to Speech", key="text-to-speech-btn"):
+            generate_tts(summarized_text, "output.mp3")
+            st.audio("output.mp3", format="audio/mp3")
+           
+
+    
+        st.markdown('</div>', unsafe_allow_html=True)
+
+        st.markdown('<div class="subheader-title">Ask Questions from the PDF</div>', unsafe_allow_html=True)
         question = st.text_input("Enter your question:")
+        st.markdown('<div class="alert">For this option: Your text is temporarily saved as vectors for text-embedding and then deleted.<br>Your privacy is crucial for us.</div>', unsafe_allow_html=True)
+        
+        st.markdown("<br>", unsafe_allow_html=True)
+        
+        documents = chunk_by_max_chunk_size(content, 200, preserve_sentence_structure=True)
+        text_chunks = [chunk.text for chunk in documents.chunks]
 
-
-        documents = chunk_by_max_chunk_size(content,200, preserve_sentence_structure=True)
-
-
-        chunks = documents.chunks
-
-        #print(documents.num_chunks)
-
-        text_chunks = [chunk.text for chunk in chunks]
-
-        embedded_documents = []
-
-        for chunk in text_chunks:
-            embedding = generate_embeddings_open_ai(chunk)
-            embedded_documents.append(embedding)
-
-
+        embedded_documents = [generate_embeddings_open_ai(chunk) for chunk in text_chunks]
 
         db = VectorDatabase('data')
-        # Adding each vector to the database with some metadata (e.g., the document it came from)
         for idx, emb in enumerate(embedded_documents):
             db.add_vector(emb, {"doc_id": idx, "vector": text_chunks[idx]}, normalize=True)
 
-
         db.save_to_disk("rag")
-
-
         db.load_from_disk("rag")
 
-
-        if st.button("Get Answer"):
+        if st.button("Get Answer", key="get-answer-btn"):
             try:
                 query_embedding = generate_embeddings_open_ai(question)
-                query_embedding = db.normalize_vector(query_embedding)  # Normalizing the query vector
-
-                # Debugging: Print the query embedding to ensure it's generated correctly
-                st.write(f"Query Embedding: {query_embedding}")
+                query_embedding = db.normalize_vector(query_embedding)
 
                 results = db.top_cosine_similarity(query_embedding, top_n=1)
                 
                 if results:
                     context = results[0][0]['vector']
                     prompt = f"Answer the following question: {question} \n Based on this context only: \n{context}"
-                    
-                    # Debugging: Print the context to ensure it's retrieved correctly
-                    st.write(f"Context: {context}")
-
                     answer = generate_text(prompt)
                 else:
                     answer = "No relevant context found in the PDF."
@@ -209,6 +191,8 @@ if uploaded_file is not None:
 else:
     st.info("Please upload a file to summarize.")
 
-# Add a footer image
-footer_image = Image.open("lip6.png")  
-st.image(footer_image, use_column_width=True, caption="Thank you for using our service!")
+# Footer section
+with st.container():
+    footer_image = Image.open("lip6.png")
+    st.image(footer_image, use_column_width=True, caption="Thank you for using our service!")
+    st.markdown('<div class="footer-text centered-footer">© 2024 Laboratoire d\'Informatique de Paris 6, CNRS, Sorbonne Universités. All rights reserved</div>', unsafe_allow_html=True)
